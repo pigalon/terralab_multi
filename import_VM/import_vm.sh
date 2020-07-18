8bf3e65239ea302300d853f840ca64be6694154a
pierrick <pierrick@pop-os.localdomain>
2020-07-18 15:39:58 +0200

    import VM

---
+++ import_VM/import_vm.sh
@@ -0,0 +1,1 @@
+aws ec2 import-image --description "W10 VM" --disk-containers "file://w10_containers.json"
---
+++ import_VM/monitoring_vm_creating.sh
@@ -0,0 +1,1 @@
+aws ec2 describe-import-image-tasks --import-task-ids import-ami-0c82a336bd4b909ca
