a10e0598b1cc802e8b74824dd8b6e1356fa32602
pierrick <pierrick@pop-os.localdomain>
2020-08-12 08:16:00 +0200

    fix ssh ip

--- moodle/instance.tf
+++ moodle/instance.tf
@@ -56,7 +56,7 @@ resource "aws_security_group" "moodle" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
