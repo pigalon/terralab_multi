12aa74eb7a521cec7ed446dc8236e0dbaff5edf6
pierrick <pierrick@pop-os.localdomain>
2020-09-20 23:33:55 +0200

    comment instance for private sub

--- security_lab_v4/instance.tf
+++ security_lab_v4/instance.tf
@@ -1,122 +1,122 @@
-provider "aws" {
-    profile    = "ludo"
-    region     = "eu-west-3"
-}
+# provider "aws" {
+#     profile    = "ludo"
+#     region     = "eu-west-3"
+# }
 
-resource "aws_instance" "winserver2016" {
-    key_name      = "lci"
-    ami           = "ami-035886e9921128d1a"
-    instance_type = "t2.micro"
+# resource "aws_instance" "winserver2016" {
+#     key_name      = "lci"
+#     ami           = "ami-035886e9921128d1a"
+#     instance_type = "t2.micro"
 
-    connection {
-        type        = "ssh"
-        user        = "ubuntu"
-        private_key = file("~/.ssh/lci")
-    }
+#     connection {
+#         type        = "ssh"
+#         user        = "ubuntu"
+#         private_key = file("~/.ssh/lci")
+#     }
 
-  # the VPC subnet
-  subnet_id = aws_subnet.wan.id
+#   # the VPC subnet
+#   subnet_id = aws_subnet.wan.id
 
-  # the security group
-  vpc_security_group_ids = [aws_security_group.wan.id]
+#   # the security group
+#   vpc_security_group_ids = [aws_security_group.wan.id]
 
-    tags = {
-        Name  = "machine_formateur"
-    }
-}
+#     tags = {
+#         Name  = "machine_formateur"
+#     }
+# }
 
-resource "aws_network_interface" "winserver2016" {
-    subnet_id       = aws_subnet.wan.id
-    private_ips   = ["172.16.10.12"]
-    security_groups = [aws_security_group.wan.id]
+# resource "aws_network_interface" "winserver2016" {
+#     subnet_id       = aws_subnet.wan.id
+#     private_ips   = ["172.16.10.12"]
+#     security_groups = [aws_security_group.wan.id]
 
-    attachment {
-        instance     = aws_instance.winserver2016.id
-        device_index = 1
-    }
-}
+#     attachment {
+#         instance     = aws_instance.winserver2016.id
+#         device_index = 1
+#     }
+# }
 
-resource "aws_instance" "kali" {
-    key_name      = "lci"
-    ami           = "ami-0e2cff13c93b60699"
-    instance_type = "t2.micro"
+# resource "aws_instance" "kali" {
+#     key_name      = "lci"
+#     ami           = "ami-0e2cff13c93b60699"
+#     instance_type = "t2.micro"
     
-    connection {
-        type        = "ssh"
-        user        = "ubuntu"
-        private_key = file("~/.ssh/lci")
-    }
+#     connection {
+#         type        = "ssh"
+#         user        = "ubuntu"
+#         private_key = file("~/.ssh/lci")
+#     }
 
-  # the VPC subnet
-  subnet_id = aws_subnet.wan.id
+#   # the VPC subnet
+#   subnet_id = aws_subnet.wan.id
 
-  # the security group
-  vpc_security_group_ids = [aws_security_group.wan.id]
-
-
-    tags = {
-        Name  = "machine_kali"
-    }
-}
-
-resource "aws_network_interface" "kali" {
-    subnet_id       = aws_subnet.wan.id
-    private_ips   = ["172.16.10.11"]
-    security_groups = [aws_security_group.wan.id]
-
-    attachment {
-        instance     = aws_instance.kali.id
-        device_index = 1
-    }
-}
+#   # the security group
+#   vpc_security_group_ids = [aws_security_group.wan.id]
 
 
+#     tags = {
+#         Name  = "machine_kali"
+#     }
+# }
 
-resource "aws_instance" "pfsense" {
-    key_name      = "lci"
-    ami           = "ami-0ec111d72b25692e3"
-    instance_type = "t2.nano"
+# resource "aws_network_interface" "kali" {
+#     subnet_id       = aws_subnet.wan.id
+#     private_ips   = ["172.16.10.11"]
+#     security_groups = [aws_security_group.wan.id]
 
-    connection {
-        type        = "ssh"
-        user        = "ubuntu"
-        private_key = file("~/.ssh/lci")
-    }
+#     attachment {
+#         instance     = aws_instance.kali.id
+#         device_index = 1
+#     }
+# }
 
-    network_interface {
-        network_interface_id = aws_network_interface.wan_pfsense.id
-				device_index         = 0
-    }
 
-    network_interface {
-        network_interface_id = aws_network_interface.lan_pfsense.id
-        device_index         = 1
-    }
 
-    tags = {
-        Name  = "machine_pfsense"
-    }
-}
+# resource "aws_instance" "pfsense" {
+#     key_name      = "lci"
+#     ami           = "ami-0ec111d72b25692e3"
+#     instance_type = "t2.nano"
 
-resource "aws_network_interface" "wan_pfsense" {
-    subnet_id       = aws_subnet.wan.id
-    security_groups = [aws_security_group.wan.id]
-    private_ips      = ["172.16.10.10"]
+#     connection {
+#         type        = "ssh"
+#         user        = "ubuntu"
+#         private_key = file("~/.ssh/lci")
+#     }
 
-    tags = {
-        Name = "default"  #This will be the **Eth0** tag !!
-    }
-}
+#     network_interface {
+#         network_interface_id = aws_network_interface.wan_pfsense.id
+# 				device_index         = 0
+#     }
 
-resource "aws_network_interface" "lan_pfsense" {
-	subnet_id       = aws_subnet.lan.id
-	security_groups = [aws_security_group.lan.id]
-	private_ips      = ["172.16.11.10"]
+#     network_interface {
+#         network_interface_id = aws_network_interface.lan_pfsense.id
+#         device_index         = 1
+#     }
 
-	tags = {
-			Name = "default"
-	}
-}
+#     tags = {
+#         Name  = "machine_pfsense"
+#     }
+# }
+
+# resource "aws_network_interface" "wan_pfsense" {
+#     subnet_id       = aws_subnet.wan.id
+#     security_groups = [aws_security_group.wan.id]
+#     private_ips      = ["172.16.10.10"]
+
+#     tags = {
+#         Name = "default"  #This will be the **Eth0** tag !!
+#     }
+# }
+
+# resource "aws_network_interface" "lan_pfsense" {
+# 	subnet_id       = aws_subnet.lan.id
+# 	security_groups = [aws_security_group.lan.id]
+# 	private_ips      = ["172.16.11.10"]
+
+# 	tags = {
+# 			Name = "default"
+# 	}
+# }
 
 
 resource "aws_instance" "win10" {
