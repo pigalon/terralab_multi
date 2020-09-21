2e20d8b1b71db4ae08ab959efc5789a45447de57
pierrick <pierrick@pop-os.localdomain>
2020-09-21 09:25:18 +0200

    vpc peering fix

--- security_lab_v4/vpc.tf
+++ security_lab_v4/vpc.tf
@@ -27,18 +27,18 @@ resource "aws_vpc" "lan-lab-1" {
 resource "aws_vpc_peering_connection" "pub-wan" {
   peer_vpc_id   = aws_vpc.wan-lab-1.id
   vpc_id        = aws_vpc.lab-1.id
-  peer_region   = "us-east-1"
+  peer_region   = "eu-west-3"
 }
 
 resource "aws_vpc_peering_connection" "pub-lan" {
   peer_vpc_id   = aws_vpc.lan-lab-1.id
   vpc_id        = aws_vpc.lab-1.id
-  peer_region   = "us-east-1"
+  peer_region   = "eu-west-3"
 }
 
 resource "aws_vpc_peering_connection" "wan-lan" {
   peer_vpc_id   = aws_vpc.wan-lab-1.id
   vpc_id        = aws_vpc.lan-lab-1.id
-  peer_region   = "us-east-1"
+  peer_region   = "eu-west-3"
 }
 
