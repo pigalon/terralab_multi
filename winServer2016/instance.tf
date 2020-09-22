c16489cf3e8b12b70795415f2b2c164591b41108
pierrick <pierrick@pop-os.localdomain>
2020-09-22 23:42:45 +0200

    up gp

--- winServer2016/instance.tf
+++ winServer2016/instance.tf
@@ -19,6 +19,32 @@ resource "aws_instance" "winserver2016" {
   }
 
     # the security group
-    vpc_security_group_ids = [aws_security_group.lab_docker.id]
+    vpc_security_group_ids = [aws_security_group.win2016-gp.id]
 
+}
+
+resource "aws_security_group" "win2016-gp" {
+    name        = "win2016-gp"
+    description = "security group that allows ssh and all egress traffic"
+    
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["82.64.103.42/32"]
+    }
+
+    ingress {
+        from_port   = 3389
+        to_port     = 3389
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }     
 }
